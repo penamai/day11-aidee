ed2e721a7e8bc6c596f901d49a4d5c9a15927965
Guile <jimenezguiles7255@gmail.com>
2023-08-29 17:17:33 +0800

    FEAT: Add strike through method

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,15 +2,15 @@ import { useDispatch } from "react-redux";
 import { toggleDone } from "./todoListSlice";
 
 const TodoItem = (props) =>{
-    const done = props.done ? "✔" : "✖";
+    const style = props.done ? {textDecoration: "line-through"}: {};
     const dispatch = useDispatch();
-
     const handleClick = () =>{
         dispatch(toggleDone(props.id))
-    }
 
+    }
     return(
-        <div className="TodoItem" onClick={handleClick}>{props.item} {done}</div>
+        
+        <div className="TodoItem" onClick={handleClick}><span style={style}>{props.item}</span></div>
     );
 }
 
