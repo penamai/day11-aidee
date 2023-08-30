a670b47a3f75ffcacd4cf8b2dc8e16d4b43b0287
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-30 11:05:59 +0800

    feat: added a router

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from "./app/store.js";
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App />
+}]);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </ Provider>
   </React.StrictMode>
 );
