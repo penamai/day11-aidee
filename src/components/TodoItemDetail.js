6b84cfc1fb6643c87ba59a5044c8c7819c47e6cc
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-30 14:11:31 +0800

    feat: added css style for todo item details

--- src/components/TodoItemDetail.js
+++ src/components/TodoItemDetail.js
@@ -1,3 +1,4 @@
+import '../css/TodoItemDetails.css';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
@@ -5,7 +6,7 @@ const TodoItemDetail = () => {
     const { id } = useParams();
     const todoItem = useSelector(state => state.todo.todoList.find(todo => todo.id === id));
     return(
-        <div>
+        <div className="todoItemDetail">
             <h1>Todo Item Detail</h1>
             <div>Item ID: {todoItem?.id}</div>
             <div>Item Text: {todoItem?.text}</div>
